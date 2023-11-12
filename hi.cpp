7e9ff8a0edd43ee1b9f068a62b0abbf3fb2605ac
Varun Girish <varungirish0905@gmail.com>
2023-11-11 18:08:30 -0800

    Made corrections

--- hi.cpp
+++ hi.cpp
@@ -1,5 +1,8 @@
-#include<stdio.h>
-printf("hello World");
-printf("Second commit edits");
-printf("This is the remote editing on this file");
-printf("This is the third edit");
+#include <iostream>
+
+int main(){
+std::cout << "hello World" << std::endl;
+std::cout << "Second commit edits" << std::endl;
+std::cout << "This is the remote editing on this file" << std::endl;
+std::cout << "This is the third edit" << std::endl;
+}
